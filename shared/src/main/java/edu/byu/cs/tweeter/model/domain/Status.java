7cffb60d220a50551a60be6f9323bb0ec8a3eb45
MichaelCh-svg <champeniosmichael@gmail.com>
2023-01-23 17:35:29 -0600

    Status toString function fixed so that 'datetime' shows as 'timestamp'.

--- shared/src/main/java/edu/byu/cs/tweeter/model/domain/Status.java
+++ shared/src/main/java/edu/byu/cs/tweeter/model/domain/Status.java
@@ -93,7 +93,7 @@ public class Status implements Serializable {
         return "Status{" +
                 "post='" + post + '\'' +
                 ", user=" + user +
-                ", datetime=" + timestamp +
+                ", timestamp=" + timestamp +
                 ", mentions=" + mentions +
                 ", urls=" + urls +
                 '}';
